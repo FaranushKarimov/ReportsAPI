21b568f8de0c6d0c7ea6ca1df92f09082307d8d1
Alex Wexov <wexouv@gmail.com>
2022-01-26 22:02:36 +0500

    Added WorkerBase template

---
+++ Worker/Program.cs
@@ -0,0 +1,9 @@
+using ReportsAPI.Worker; 
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
