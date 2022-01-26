bea5ea8b6beea60b18d48782c3536e10685ecebe
FaranushKarimov <faranush.karimov@gmail.com>
2022-01-26 14:48:24 +0300

    Changes in project

---
+++ Contracts/Services/IIncomeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Services
+{
+    public interface IIncomeService
+    {
+        Task UpdateAsync();
+    }
+}
---
+++ Services/IncomeService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class IncomeService : IIncomeService
+    {
+        public Task UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
