d38e107041575e21c7d68ccc765b6e4b90c38ee1
Alex Wexov <wexouv@gmail.com>
2022-01-27 14:20:50 +0500

    Added income

--- Services/IncomeService.cs
+++ Services/IncomeService.cs
@@ -3,14 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace Services
 {
     public class IncomeService : IIncomeService
     {
-        public Task UpdateAsync()
+        public async Task UpdateAsync()
         {
-            throw new NotImplementedException();
+            var client = new RestClient($"https://localhost:5001/Wildberries/GetIncomes");
+            var request = new RestRequest($"https://localhost:5001/Wildberries/GetIncomes", Method.Get);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            Console.WriteLine(response.ErrorMessage);
+            Console.WriteLine(response.StatusDescription);
         }
     }
 }
